fe8dfd3d98029b4f8c69c7b42baaaedd5feeb8e7
Kushal <kushalkhandelwal27@gmail.com>
2021-11-10 01:57:16 +0530

    Fixed password field

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-
-
 const loginButton=document.getElementById("loginButton");
+const passwordField=document.getElementById("inputPassword")
+
 
 
 loginButton.onclick = async ()=>{
     let password = document.getElementById("inputPassword").value
     let username= document.getElementById("userName").value
-    const res = await axios.post('https://k4shal.pythonanywhere.com/api/login', { username: username, password: password });
-    console.log(res);
+
+    if(!password || !username){
+        alert("Enter Password and Username")
+        return 
+    }
+    const res = await axios.post('https://k4shal.pythonanywhere.com/api/login',{ username: username, password: password });
+    alert(res.data)
 }
 
